6f9f4816cfd91c01aa2da2a5ab8ea1a5baa8aae9
Gulabsingh008 <gulabsinghrajput008@gmail.com>
2024-08-23 21:12:07 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,7 +8,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "21784817"))
     API_HASH = getenv("API_HASH", "c8209405e82ac1d833e653d0330bbf17")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:7003534933:AAFexthFi2eV_OTZEtJZ1tqNjj3tzIdwpCU")
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
     FSUB = getenv("FSUB", "Ra_yan_2024_movie")
     CHID = int(getenv("CHID", "-1002172671575"))
     SUDO = list(map(int, getenv("SUDO", "1114789110").split()))

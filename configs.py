4bb25f04b22f346e5d4e035bb9d6009d1131b67d
Gulabsingh008 <gulabsinghrajput008@gmail.com>
2024-08-23 21:19:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,11 +8,11 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "21784817"))
     API_HASH = getenv("API_HASH", "c8209405e82ac1d833e653d0330bbf17")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7003534933:AAFexthFi2eV_OTZEtJZ1tqNjj3tzIdwpCU")
     FSUB = getenv("FSUB", "Ra_yan_2024_movie")
     CHID = int(getenv("CHID", "-1002172671575"))
     SUDO = list(map(int, getenv("SUDO", "1114789110").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://naturecreation277:UXhOlYWOIOqINmSp@cluster0.wjssof4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
 
